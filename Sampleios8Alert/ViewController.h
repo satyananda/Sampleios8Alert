e56aba66550bd85ba8ac7c1d89f2d0557620cb83
Satyanand <SatyanandaKumar@Satyanandas-Mac-mini.local>
2015-01-08 20:03:38 +0530

    Initial Commit

---
+++ Sampleios8Alert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sampleios8Alert
+//
+//  Created by Satyanand on 1/8/15.
+//  Copyright (c) 2015 prokarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sampleios8Alert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sampleios8Alert
+//
+//  Created by Satyanand on 1/8/15.
+//  Copyright (c) 2015 prokarma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
